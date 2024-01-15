3fb26bd460cafebcdefc9821870c7408b051fe90
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-01-15 10:29:19 +0100

    Ausgabe 1

---
+++ src/ausgabe/Ausgabe1.java
@@ -0,0 +1,10 @@
+package ausgabe;
+
+public class Ausgabe1 {
+
+	public static void main(String[] args) {
+		System.out.println("Ausgabe 1");
+
+	}
+
+}
---
+++ src/ausgabe/Ausgabe2.java
@@ -0,0 +1,5 @@
+package ausgabe;
+
+public class Ausgabe2 {
+
+}
