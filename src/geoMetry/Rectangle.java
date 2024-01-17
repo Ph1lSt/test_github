c66957fbc989b92f66fc17dd509f4f203a2577f6
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-01-17 10:52:53 +0100

    Nvm, calculateArea auscodiert

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -15,16 +15,15 @@ public class Rectangle extends Shapes{
 
 	@Override
 	public double calculateCircumference() {
-		double x = this.coordinate1.getX() - this.coordinate2.getX();
-		double y = this.coordinate1.getY() - this.coordinate2.getX();
-		circumference = x *y;
-		return circumference;
+		return 0;
 	}
 
 	@Override
 	public double calculateArea() {
-		// TODO Auto-generated method stub
-		return 0;
+		double x = this.coordinate1.getX() - this.coordinate2.getX();
+		double y = this.coordinate1.getY() - this.coordinate2.getX();
+		circumference = x *y;
+		return circumference;
 	}
 	
 	
