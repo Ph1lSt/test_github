36f4cf28b619de4a18c32c63418dac8f9ca9c2f1
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-01-17 10:55:42 +0100

    JETZT, calculateAre programmiert

--- src/geoMetry/Rectangle.java
+++ src/geoMetry/Rectangle.java
@@ -5,25 +5,33 @@ public class Rectangle extends Shapes{
 	Coordinate coordinate1;
 	Coordinate coordinate2;
 	
+	double x = this.coordinate1.getX() - this.coordinate2.getX();
+	double y = this.coordinate1.getY() - this.coordinate2.getX();
+	
 	double circumference;
+	double area;
 	
 	public Rectangle(Coordinate coordinate1, Coordinate coordinate2) {
 		super();
 		this.coordinate1 = coordinate1;
 		this.coordinate2 = coordinate2;
 	}
-
+	/**
+	 * calculates the circumference of the rectangle
+	 */
 	@Override
 	public double calculateCircumference() {
-		return 0;
+		circumference = 2*x + 2*y;
+		return circumference;
 	}
 
+	/**
+	 * calculates the area of the rectangle
+	 */
 	@Override
 	public double calculateArea() {
-		double x = this.coordinate1.getX() - this.coordinate2.getX();
-		double y = this.coordinate1.getY() - this.coordinate2.getX();
-		circumference = x *y;
-		return circumference;
+		area = x *y;
+		return area;
 	}
 	
 	
