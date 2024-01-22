a871bd1c19defcd3c29ceb9e9222fd4f16215ff9
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-01-22 08:09:18 +0100

    Kommentare - Triangle

--- src/geoMetry/Triangle.java
+++ src/geoMetry/Triangle.java
@@ -18,32 +18,16 @@ public class Triangle extends Shapes {
         };
     }
 	
+    /*
+     * Calculates the individual lengths
+     */
 	public double calculateSide(Coordinate point1, Coordinate point2) {
         return Math.sqrt(Math.pow(point2.getX() - point1.getX(), 2) + Math.pow(point2.getY() - point1.getY(), 2));
     }
 
-	
-//	public double calculateSide(int side) {
-//		switch(side) {
-//		case 0 : this.sideLengths[0] = Math.sqrt(Math.pow(this.point2.getX() - this.point1.getX(), 2) +
-//				Math.pow(this.point2.getY() - this.point1.getY(), 2));
-//				return this.sideLengths[side];
-//				
-//		case 1 : this.sideLengths[1] = Math.sqrt(Math.pow(this.point3.getX() - this.point2.getX(), 2) +
-//				Math.pow(this.point3.getY() - this.point2.getY(), 2));
-//				return this.sideLengths[side];
-//				
-//		case 2 : this.sideLengths[2] = Math.sqrt(Math.pow(this.point1.getX() - this.point3.getX(), 2) +
-//				Math.pow(this.point1.getY() - this.point3.getY(), 2));
-//				return this.sideLengths[side];
-//				
-//		default : System.err.println("Fehler!"); break;
-//		}
-//		return this.sideLengths[side];
-//		
-//	}
-	
-	
+	/*
+	 * Calculates the circumference of the triangle
+	 */
 	@Override
 	public double calculateCircumference() {
 		double circumference = 0;
@@ -53,6 +37,9 @@ public class Triangle extends Shapes {
         return circumference;
 		}
 
+	/*
+	 * calculates the area of the triangle
+	 */
 	@Override
 	public double calculateArea() {
 		double s = calculateCircumference() / 2;
