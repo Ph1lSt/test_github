2a68c8e6638991e6bc4b1e7ca50744339c929f0d
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-02-06 08:35:03 +0100

    Implementing of method searchNewsFeed

--- src/theSocialNetwork/NewsFeed.java
+++ src/theSocialNetwork/NewsFeed.java
@@ -48,7 +48,22 @@ public class NewsFeed {
 		}
 	}
 	
-	
+	/**
+	 * Searches the news feed for posts of a certain user
+	 * @param user
+	 */
+	public void searchNewsFeed(User user) {
+		boolean foundResult = false;
+		for(Post p : posts) {
+			if(p.getAuthor().equals(user)) {
+				System.out.println(p.toString());
+				foundResult = true;
+			}
+		}
+		if(foundResult == false) {
+			System.out.println("Keine Ergebnisse gefunden...");
+		}
+	}
 	
 	
 }
--- src/theSocialNetwork/Test.java
+++ src/theSocialNetwork/Test.java
@@ -5,6 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		NewsFeed n = new NewsFeed();
 		User u = new User("Tom");
+		User u2 = new User("Kowalski");
 		Message m = new Message(u, "Hello");
 		m.addMessage(m);
 		Message m2 = new Message(u, "Bye");
@@ -16,8 +17,9 @@ public class Test {
 		n.addPost(m2);
 		n.addPost(p);
 		n.printNewsFeed();
-		n.deletePost(m2);
-		n.deletePost(p);
-		n.printNewsFeed();
+//		n.deletePost(m2);
+//		n.deletePost(p);
+//		n.printNewsFeed();
+		n.searchNewsFeed(u2);
 	}
 }
