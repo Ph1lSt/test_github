8f8b46b5cfdb7733fa362345e6f0e7132b869671
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-03-04 11:44:38 +0100

    getter setter lines

--- src/arrays/ArrayListAufgabe1.java
+++ src/arrays/ArrayListAufgabe1.java
@@ -1,11 +1,26 @@
 package arrays;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class ArrayListAufgabe1 {
+	private List<String>lines;
 
 	public static void reduceToComments(List<String>lines) {
 		
 	}
+	
+	ArrayListAufgabe1(){
+		this.setLines(new ArrayList<String>());
+	}
+
+	public List<String> getLines() {
+		return lines;
+	}
+
+	public void setLines(List<String> lines) {
+		this.lines = lines;
+	}
+	
 	
 }
