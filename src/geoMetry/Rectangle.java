7ded5a0e61003a270f062be563978490fd695d16
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-03-04 09:48:41 +0100

    idk

---
+++ src/geoMetry/Rectangle.java
@@ -0,0 +1,21 @@
+package geoMetry;
+
+public class Rectangle extends Shapes{
+
+	public Rectangle(double x, double y) {
+		super(x, y);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public double calculateCircumference() {
+		return 0;
+	}
+
+	@Override
+	public double calculateArea() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
--- src/geoMetry/Shapes.java
+++ src/geoMetry/Shapes.java
@@ -1,13 +1,45 @@
 package geoMetry;
 import java.util.ArrayList;
 
-public abstract class Shapes {
-
-	private ArrayList<Coordinate> coordinates = new ArrayList<Coordinate>();
+public abstract class Shapes extends Coordinate {
+	
+	
+	public Shapes(double x, double y) {
+		super(x, y);
+	}
+	
+	private ArrayList<Coordinate> coordinates;
 	double circumference;
 	double area;
 	int coordinateAmount = coordinates.size();
 	
 	public abstract double calculateCircumference();
 	public abstract double calculateArea();
+	public ArrayList<Coordinate> getCoordinates() {
+		return coordinates;
+	}
+	
+	public void setCoordinates(ArrayList<Coordinate> coordinates) {
+		this.coordinates = coordinates;
+	}
+	public double getCircumference() {
+		return circumference;
+	}
+	public void setCircumference(double circumference) {
+		this.circumference = circumference;
+	}
+	public double getArea() {
+		return area;
+	}
+	public void setArea(double area) {
+		this.area = area;
+	}
+	public int getCoordinateAmount() {
+		return coordinateAmount;
+	}
+	public void setCoordinateAmount(int coordinateAmount) {
+		this.coordinateAmount = coordinateAmount;
+	}
+	
+	
 }
