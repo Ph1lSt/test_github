5807a43ae5a7ca3bfa203423fa6275cbb8b01acf
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-03-05 08:57:31 +0100

    LinkedListTester

--- src/linkedList/LinkedListTester.java
+++ src/linkedList/LinkedListTester.java
@@ -1,5 +1,6 @@
 package linkedList;
 
+import java.util.Collections;
 import java.util.Iterator;
 import java.util.LinkedList;
 import java.util.List;
@@ -7,14 +8,16 @@ import java.util.List;
 public class LinkedListTester {
 
 	public static void main(String[] args) {
-
+		
 		List<Integer> list = new LinkedList<Integer>();
 		list.add(1);
 		list.add(2);
 		list.add(3);
 		
+		Collections.rotate(list, 2);
+		
 		//"Laufvariable" zum Iterieren einer Datenstruktur
-		//läuft immer vom Anfang bis zum ende
+		//läuft immer vom Anfang bis zum Ende
 		Iterator<Integer> iterator = list.iterator();
 		while(iterator.hasNext()) {
 			System.out.println(iterator.next());
