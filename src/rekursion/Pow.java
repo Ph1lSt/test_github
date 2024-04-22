754fd5353ef29b9b20ef6e194f78feb8daeee8cd
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-04-22 11:00:23 +0200

    idk

---
+++ src/rekursion/Pow.java
@@ -0,0 +1,18 @@
+package rekursion;
+
+public class Pow {
+	
+	public static double pow(double base, double exponent) {
+		if(exponent==0) {
+			return 1;
+		}else {
+			return Math.pow(base, exponent);
+		}
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
--- src/stack/UPNTaschenrechner.java
+++ src/stack/UPNTaschenrechner.java
@@ -1,4 +1,4 @@
-package tasks;
+package stack;
 
 import java.util.Scanner;
 
