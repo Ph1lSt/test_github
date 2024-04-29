762915b762d27605b126da93f280691999c4961d
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-04-29 07:46:19 +0200

    Changes

--- src/excursion/SumOfTheDigits.java
+++ src/excursion/SumOfTheDigits.java
@@ -1,35 +1,19 @@
 package excursion;
 
-import java.util.Scanner;
-
 public class SumOfTheDigits {
-	
-    public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        
-        System.out.println("Geben Sie eine number ein:");
-        String numberString = scanner.nextLine();
-        
-        int quersumme = berechneQuersumme(numberString);
-        
-        System.out.println("Die Quersumme von " + numberString + " ist: " + quersumme);
-        
-        scanner.close();
+ 
+    public static int digitSum(long value) {
+        int sum = 0;
+        while (value != 0) {
+            sum += value % 10;
+            value /= 10;
+        }
+        return sum;
     }
     
-    public static int berechneQuersumme(String numberString) {
-        int quersumme = 0;
-        for (int i = 0; i < numberString.length(); i++) {
-            char c = numberString.charAt(i);
-            if (Character.isDigit(c)) {
-                int number = Character.getNumericValue(c);
-                quersumme += number;
-            } else {
-                System.out.println("Ungültiges Zeichen gefunden: " + c);
-                return -1; // Falls eine ungültige Zeichen gefunden wird
-            }
-        }
-        return quersumme;
+  
+    public static int digitSum(String value) {
+        long longValue = Long.parseLong(value);
+        return digitSum(longValue);
     }
 }
-
