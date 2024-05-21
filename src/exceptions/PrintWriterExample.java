fc6804f76a58284459363e07d75d220a5e1a81d2
Ph1lSt <philipp.steiner@hak-reutte.ac.at>
2024-05-21 13:01:17 +0200

    printWriter

---
+++ src/exceptions/PrintWriterExample.java
@@ -0,0 +1,17 @@
+package exceptions;
+
+import java.io.FileNotFoundException;
+import java.io.PrintWriter;
+import java.time.LocalDateTime;
+
+public class PrintWriterExample {
+    public static void main(String[] args) throws FileNotFoundException {
+        try {
+            PrintWriter printWriter = new PrintWriter("test.txt");
+            printWriter.println(LocalDateTime.now().toString());
+            printWriter.close();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
